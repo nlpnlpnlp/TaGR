8a559aaa1aedcefcfef42474c225ca35c8210550
CNTrustNLP <122439000+nlpnlpnlp@users.noreply.github.com>
2025-10-08 15:55:51 +0800

    Add files via upload

---
+++ scripts/run_cora_TaGR.sh
@@ -0,0 +1,29 @@
+
+data_type='cora'
+max_len=512
+
+epochs=600
+gpu=3
+cls_lambda=1.0 
+sparsity_lambda=1.0 
+continuity_lambda=1.0
+
+for graph_backbone in "GAT" "MLP" "GCN" "GIN" "SAGE" 
+do
+        log_dir=./logging/$data_type/$graph_backbone/
+        mkdir -p $log_dir
+        python -u main_tagr.py --max_len $max_len --embedding_dim 100 --hidden_dim 128 \
+        --epochs $epochs --lr 0.0002 \
+        --gpu $gpu \
+        --graph_backbone $graph_backbone \
+        --gnn_layers 2 \
+        --data_type $data_type \
+        --sparsity_percentage 0.5 \
+        --cls_lambda $cls_lambda \
+        --sparsity_lambda $sparsity_lambda \
+        --continuity_lambda $continuity_lambda  > $log_dir/cmd.log
+
+done
+
+
+
